c6f632fdd7d21059b836b2d3d26e9e681c787c55
Anurag22060 <anuragsawal03@gmail.com>
2023-10-19 16:13:43 +0530

    first commit

---
+++ 1.Brand_Page/src/components/Hero.jsx
@@ -0,0 +1,31 @@
+const HeroSection =() =>{
+    return(
+<main className="hero container ">
+<div className="hero-content">
+    <h1>YOUR FEET 
+DESERVE
+THE BEST</h1>
+    <p>YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR SHOES.</p>
+    <div className="hero-btn"><button>Shop Now</button>
+
+    <button className="secondary-btn">Category</button></div>
+    
+    <div className="shopping">
+        <p>Also Available On</p>
+    </div>
+    <div className="brand-icon">
+        <img src="/images/flipkart.png" alt="flipkart-logo" />
+        <img src="/images/amazon.png" alt="amazon-logo" />
+    </div>
+    
+</div>
+<div className="img1"><img className="img2" src="/images/shoe_image.png" alt="" srcset="" /></div>
+
+
+
+</main>
+    );
+    
+}
+
+export default HeroSection;
